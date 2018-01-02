a8a41f6e136e6266953fdb7b1b5fafc9b59d7082
prathyushamandala <prathyushamandala@yahoo.com>
2018-01-01 23:06:52 -0600

    equals program first commit

---
+++ Equals/src/com/visam/javatraining/equals/BankAccount.java
@@ -0,0 +1,33 @@
+package com.visam.javatraining.equals;
+
+public class BankAccount {
+	
+//	int accountNumber;
+//	float accountBalance;
+	boolean accountStatus;
+	String name;
+	
+	
+	
+	public boolean equals(Object obj) {
+		
+		BankAccount bankaccount = (BankAccount) obj;
+		if (name == null) {
+			if (bankaccount.name != null)
+				return false;
+		} else if (!name.equals(bankaccount.name))
+			return false;
+		return true;
+		
+	}
+	
+
+	public boolean status(BankAccount account) {
+		
+		if(accountStatus!=account.accountStatus) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ Equals/src/com/visam/javatraining/equals/EqualsProgram.java
@@ -0,0 +1,31 @@
+package com.visam.javatraining.equals;
+
+
+public class EqualsProgram {
+
+	public static void main(String[] args) {
+
+		BankAccount account1  = new BankAccount();
+		BankAccount account2  = new BankAccount();
+		
+		account1.name = "prathyusha";
+		account2.name = "prathyusha";
+		
+		
+		account1.accountStatus = true;
+		account2.accountStatus = true;
+		
+		
+		 account1.status(account2);
+		
+		
+		if(account1.equals(account2)) {
+			System.out.println("Bank accounts are same");
+		}else {
+			System.out.println("Bank accounts are different");
+		}
+	}
+
+}
+
+
